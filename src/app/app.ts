9afea06517c3cf5088aa28d996e6383a84e11fd5
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-06-26 18:00:58 +0300

    add fontawesome, flowbite

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('social-media-app');
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
